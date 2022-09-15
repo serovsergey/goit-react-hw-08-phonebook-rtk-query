62f8ea3fe3d6183b4ff0032438d44371875a5f70
Serov Sergey <serovsergey79@gmail.com>
2022-09-15 22:50:43 +0300

    changed initial contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,9 @@ import Filter from "./Filter";
 import Section from "./Section";
 
 const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-1', name: 'Sergey Serov', number: '459-12-56' },
+  { id: 'id-2', name: 'Irina Serova', number: '443-89-12' },
+  { id: 'id-3', name: 'Vanya stupid', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
