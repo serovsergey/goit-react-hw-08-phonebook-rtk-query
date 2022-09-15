c0fc481738490301f9b7c59f19ad77a749bf0289
Serov Sergey <serovsergey79@gmail.com>
2022-09-15 22:52:56 +0300

    chahged another initial contact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ const initialContacts = [
   { id: 'id-1', name: 'Sergey Serov', number: '459-12-56' },
   { id: 'id-2', name: 'Irina Serova', number: '443-89-12' },
   { id: 'id-3', name: 'Vanya stupid', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  { id: 'id-4', name: 'Petya Smarty', number: '227-91-26' },
 ];
 
 export const App = () => {
