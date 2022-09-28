1e8c83faa1ed00bf220bc4101c794fb079a337e9
serovsergey <32988243+serovsergey@users.noreply.github.com>
2022-09-28 17:13:41 +0000

    Deploying to gh-pages from @ serovsergey/goit-react-hw-08-phonebook@4cd71ba0485a4dd319c3c260405da9dcfd0c6793 🚀

---
+++ static/js/35.10d0b46d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[35],{2035:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});r(2791);var n=r(184),c=function(){return(0,n.jsx)("div",{children:"Welcome!"})}}}]);
+//# sourceMappingURL=35.10d0b46d.chunk.js.map
